e8ab0c7ad841a193a4ea8459498f96ceeb6dfbde
Andrea Fuente <andrea.fuente.fdez.a11y@gmail.com>
2025-12-07 20:59:55 +0100

    primeros progresos

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,39 @@
+//Variables 
 
+const Siguiente = document.getElementById("next");
+const Anterior = document.getElementById("prev");
+const Reset = document.getElementById("reset");
+
+function LeerTurno () :number {
+ return Number(document.getElementById("turno-actual")?.textContent);
+}
+
+//Funciones
+function restarTurno (turnoActual: number) {
+    if (turnoActual> 0) {
+        return turnoActual - 1;
+    }
+    return 0;
+}
+
+function sumarTurno (turnoActual: number) :number {
+    if (turnoActual < 20) {
+        return turnoActual + 1;
+    }
+    return 20;
+}
+
+function resetearTurno () {
+    return 0;
+}
+
+function pintarTurno (NuevoTurno: number) :void {
+    document.getElementById("turno-actual")!.textContent = String (NuevoTurno);
+}
+}
+
+
+// Eventos
+Siguiente?.addEventListener("click", () => sumarTurno());
+Anterior?.addEventListener("click", () => restarTurno());
+Reset?.addEventListener("click", () => resetearTurno());
